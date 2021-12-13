81cb5d091c838dcd18949ac612d80f9af131baee
Umayarz18 <61628982+Umayarz18@users.noreply.github.com>
2021-12-13 00:04:28 -0800

    Update _document.js
    Added Splitbee for page analytics

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,26 +1,31 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <meta name='application-name' content='Roewyn Umayam Portfolio' />
-                    <meta name='apple-mobile-web-app-capable' content='yes' />
-                    <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-                    <link rel='mask-icon' href='/favicon.ico' />
-                    <link rel='shortcut icon' href='/favicon.ico' />
-                    <link rel="manifest" href="/manifest.json" />
-                    <link rel="apple-touch-icon" href="/favicon.ico"></link>
-                    <meta name="theme-color" content="#fff" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="application-name" content="Roewyn Umayam Portfolio" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <link rel="mask-icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/favicon.ico"></link>
+          <meta name="theme-color" content="#fff" />
+          <script async src="https://cdn.splitbee.io/sb.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
